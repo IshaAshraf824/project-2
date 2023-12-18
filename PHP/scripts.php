963d2a62511e6052ee7344aa7ed69dad698da96c
IshaAshraf824 <154266098+IshaAshraf824@users.noreply.github.com>
2023-12-19 00:12:30 +0500

    Add files via upload

---
+++ PHP/scripts.php
@@ -0,0 +1,16 @@
+<!-- javascript link -->
+<script src="js/jquery-3.2.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<!-- javascript Query Link -->
+<script src="js/jQUERY/jquery.min.js"></script>
+<script src='js/jQUERY/query.js'></script>
+<!-- <script src="js/jQUERY/query.js"></script> -->
+<script src="js/script1.js"></script>
+
+
+<!-- jq CDN -->
+
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
